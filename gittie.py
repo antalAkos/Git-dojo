77fea80b8f16a16fb70ad201f15a818911513fda
DanielKnoll <knoll.dani@gmail.com>
2017-07-11 09:47:38 +0200

    added: joke function

--- gittie.py
+++ gittie.py
@@ -1,2 +1,8 @@
 def hello():
     print("Hello, I'm Gittie!")
+
+
+def joke():
+    print("What does Batman sais to Robin when he wants to get into the Batmobil?")
+    print("???")
+    print("Come on Robint get into the Batmobil?")
